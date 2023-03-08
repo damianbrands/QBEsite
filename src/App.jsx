5fa0489f2617b8578e8fb35189acb1d934c0833d
damianbrands <daim2300@gmail.com>
2023-03-08 08:48:12 +0100

    Made necessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import './App.css'
 import Header from './components/Header'
+import TitlePage from "./components/TitlePage.jsx";
+import AboutPage from "./components/AboutPage.jsx";
+import ProjectsPage from "./components/ProjectsPage.jsx";
+import ContactPage from "./components/ContactPage.jsx";
 
 function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-    </div>
-  )
+    return (
+        <div className="App">
+            <Header></Header>
+            <TitlePage></TitlePage>
+            <AboutPage></AboutPage>
+            <ProjectsPage></ProjectsPage>
+            <ContactPage></ContactPage>
+        </div>
+    )
 }
 
 export default App
