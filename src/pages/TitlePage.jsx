f7ef0bb24f1f3444cf1173b71b53b8eb8d7d1373
damianbrands <daim2300@gmail.com>
2023-03-09 22:26:30 +0100

    Title page now fresh

---
+++ src/pages/TitlePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function TitlePage(){
+    return (
+        <div className={"title__page"}>
+            <div className={"square"}></div>
+            <div className={"title__container"}>
+                <div className={"title"}>QB Engineering</div>
+                <div className={"subtitle subtitle__title"}>Engineering done right.</div>
+            </div>
+        </div>
+    )
+}
