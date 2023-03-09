85be5aec8e6d53f39b35518594d9cf477b5e03d6
damianbrands <daim2300@gmail.com>
2023-03-09 22:47:44 +0100

    contactpage html + image added

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,7 +2,18 @@ import React from "react";
 
 export default function ContactPage(){
     return (
-        <>
-        </>
+        <div className={"contact__page"}>
+            <div className={"title page__title"}>Contact</div>
+            <div className={"contact__container"}>
+                <div>
+                    <div className={"subtitle"}>Quint Brands</div>
+                    <div className={"text"}>
+                        0612345678
+                        quint@qb
+                    </div>
+                </div>
+                <img src={"src/assets/images/quint.png"}/>
+            </div>
+        </div>
     )
 }
