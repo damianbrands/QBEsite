b9c57ab51fdd1a03a5edb9cf59d80f312efe6801
damianbrands <daim2300@gmail.com>
2023-03-09 22:26:14 +0100

    Header updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import React from "react"
 export default function Header(){
     return(
         <div className="header">
-            <div className={"logo__header"}>QB Engineering</div>
-            <div>
+            <div className={"logo__header subtitle"}>QB Engineering</div>
+            <div className={"header__button__container"}>
                 <button className={"header__button"}>About</button>
                 <button className={"header__button"}>Projects</button>
                 <button className={"header__button"}>Contact</button>
