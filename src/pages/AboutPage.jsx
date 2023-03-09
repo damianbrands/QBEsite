cd8b88419b5656314298cdfecfa0e14e88745341
damianbrands <daim2300@gmail.com>
2023-03-09 22:47:21 +0100

    aboutpage moved to pages, added html

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default function AboutPage(){
+    return(
+        <div className={"about__page"}>
+            <div className={"title page__title"}>About</div>
+            <div className={"about__container"}>
+                <div className={"subtitle"}>What we do:</div>
+                <div className={"text"}>
+                    Concept engineering
+                    Engineering
+                    Engineering Drawings
+                    Assembly
+                    Installations
+                    Services
+                </div>
+            </div>
+        </div>
+    )
+}
