a84c8dc51ce0136e077099b70e861f673fc8c981
damianbrands <daim2300@gmail.com>
2023-03-09 22:48:15 +0100

    projects page html added

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 export default function ProjectsPage(){
     return(
-        <>
-        </>
+        <div className={"projects__page"}>
+            <div className={"title page__title"}>Projects</div>
+        </div>
     )
 }
