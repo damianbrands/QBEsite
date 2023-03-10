95480f87b05d9471a758e8b340077ba214fb1000
damianbrands <daim2300@gmail.com>
2023-03-10 22:59:10 +0100

    projects id added for scroll

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function ProjectsPage(){
     return(
-        <div className={"projects__page"}>
+        <div className={"projects__page"} id={"projects"}>
             <div className={"title page__title"}>Projects</div>
         </div>
     )
