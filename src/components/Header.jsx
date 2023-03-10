126730c3c99e5d5e4136b02e5fd6be6e53350d97
damianbrands <daim2300@gmail.com>
2023-03-10 23:01:40 +0100

    Scroll in to view added on buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,25 @@
 import React from "react"
 
 export default function Header(){
+    function scrollToAbout(){
+        document.getElementById("about").scrollIntoView();
+    }
+
+    function scrollToProjects(){
+        document.getElementById("projects").scrollIntoView();
+    }
+
+    function scrollToContact(){
+        document.getElementById("contact").scrollIntoView();
+    }
+
     return(
         <div className="header">
             <div className={"logo__header subtitle"}>QB Engineering</div>
             <div className={"header__button__container"}>
-                <button className={"header__button"}>About</button>
-                <button className={"header__button"}>Projects</button>
-                <button className={"header__button"}>Contact</button>
+                <button className={"header__button"} onClick={scrollToAbout}>About</button>
+                <button className={"header__button"} onClick={scrollToProjects}>Projects</button>
+                <button className={"header__button"} onClick={scrollToContact}>Contact</button>
             </div>
         </div>
     )
