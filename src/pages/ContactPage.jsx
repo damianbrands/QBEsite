4fbae0f85aefcdbea610909c3d2857a2bf1600fd
damianbrands <daim2300@gmail.com>
2023-03-12 19:50:58 +0100

    Contact info changed to right values

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -7,8 +7,10 @@ export default function ContactPage(){
                 <div className={"title page__title"}>Contact</div>
                 <div className={"subtitle"}>Quint Brands</div>
                 <div className={"text"}>
-                    <p>0612345678</p>
-                    <p>quint@qb</p>
+                    <p>Stroeërschoolweg 4</p>
+                    <p>3776 MN Stroe</p>
+                    <p>+31 (0)6-15186121</p>
+                    <p>quint@qb-engineering.nl</p>
                 </div>
             </div>
             <img alt={"Quint Brands foto"} className={"quint"} src={"src/assets/images/quint.png"}/>
