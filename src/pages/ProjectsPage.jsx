464119ff8b2748607a855b2ea743655d442dbbf4
damianbrands <daim2300@gmail.com>
2023-03-12 19:51:21 +0100

    color changes to right colors

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -4,7 +4,7 @@ export default function ProjectsPage(){
     return(
         <>
             <div className={"projects__page"} id={"projects"}>
-                <div className={"title page__title"}>Projects</div>
+                <div className={"title page__title black"}>Projects</div>
             </div>
             <div className={"scrollDiv"} id={"contactScroll"}></div>
         </>
--- src/pages/TitlePage.jsx
+++ src/pages/TitlePage.jsx
@@ -6,8 +6,8 @@ export default function TitlePage(){
             <div className={"title__page"}>
                 <div className={"square"}></div>
                 <div className={"title__container"}>
-                    <div className={"title"}>QB Engineering</div>
-                    <div className={"subtitle subtitle__title"}>Engineering done right.</div>
+                    <div className={"title black"}>QB Engineering</div>
+                    <div className={"subtitle subtitle__title black"}>Engineering done right.</div>
                 </div>
             </div>
             <div className={"scrollDiv"} id={'aboutScroll'}></div>
